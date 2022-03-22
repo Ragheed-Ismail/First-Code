cbfa400ebfb9f537adbb34d249fd629f07bd27a5
Ragheed-Ismail <ragheed.z.ismail@simplexity.io>
2022-03-22 15:16:18 +0200

    Nothing

---
+++ First-Code.py
@@ -0,0 +1,12 @@
+print ("Hello World!")
+x = input("Enter value of x:")
+y = input("Enter value of y:")
+print (str(x)+"+"+str(y)+"="+str(x+y)+" .")
+ans = input("Is that corret?(y/n)")
+if ans==y:
+    print(":)")
+else:
+    if ans==n:
+        print(":(")
+    else:
+        print(":/")
